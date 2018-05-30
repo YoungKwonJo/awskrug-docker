d11e334d8fc9f7b429e98527b4546c18add164fe
west0706 <west0706@gmail.com>
2018-05-30 15:35:15 +0000

    test

--- node/server.js
+++ node/server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 // App 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h2>' + 'Hostname: ' + hostname + '</h2>');
+  res.send('<h2>' + 'Hostname!!!: ' + hostname + '</h2>');
 });
 
 app.listen(PORT, HOST);
