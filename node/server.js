9a673eae085942ed06d0ccfa79751e47b1259e99
YoungKwon Jo <youngkwonjo@industrysolution.co.kr>
2018-07-10 13:59:26 +0900

    test

--- node/server.js
+++ node/server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send("<html><head><h1>hostname : " + hostname + 
+  res.send("<html><head> -------------- <h1>hostname : " + hostname + 
 	"</h1></head><body><img src='https://cdn-images-1.medium.com/max/1600/0*Hl-1BN46JLiTh8UM.png' height='512' width='512'></body></html>");
 });
 
